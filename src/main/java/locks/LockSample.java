e5a5535b5312249dcc9d9e931a602f8e3dd7979f
Anton Pomanytskyi <pomanitska@ae.com>
2022-01-03 22:43:07 +0200

    Locks sample.

---
+++ src/main/java/locks/LockSample.java
@@ -0,0 +1,55 @@
+package locks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/*
+Can get the same result if use synchronized for method or inside method.
+
+WITHOUT LOCKS:
+
+Threads access to resource:Thread-0Thread-1Thread-4
+Threads access to resource:Thread-0Thread-1Thread-4Thread-3Thread-2
+Threads access to resource:Thread-0Thread-1Thread-4Thread-3
+Threads access to resource:Thread-0Thread-1Thread-4Thread-3
+Threads access to resource:Thread-0Thread-1Thread-4Thread-3
+
+
+WITH LOCKS:
+
+Threads access to resource:Thread-1
+Threads access to resource:Thread-1Thread-0
+Threads access to resource:Thread-1Thread-0Thread-2
+Threads access to resource:Thread-1Thread-0Thread-2Thread-3
+Threads access to resource:Thread-1Thread-0Thread-2Thread-3Thread-4
+ */
+public class LockSample {
+
+    static Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(LockSample::accessResource);
+        Thread t2 = new Thread(LockSample::accessResource);
+        Thread t3 = new Thread(LockSample::accessResource);
+        Thread t4 = new Thread(LockSample::accessResource);
+        Thread t5 = new Thread(LockSample::accessResource);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+    }
+
+    private static void accessResource() {
+        lock.lock();
+        SomeResource.STRING_BUILDER.append(Thread.currentThread().getName());
+        System.out.println(SomeResource.STRING_BUILDER.toString());
+        lock.unlock();
+    }
+}
+
+class SomeResource {
+    static StringBuffer STRING_BUILDER = new StringBuffer("Threads access to resource:");
+}
+
