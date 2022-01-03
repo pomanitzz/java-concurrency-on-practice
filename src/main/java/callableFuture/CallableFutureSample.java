51b37da13fdb151d644c6ded79dc190d9654cf86
Anton Pomanytskyi <pomanitska@ae.com>
2022-01-03 18:11:26 +0200

    CompletableFuture execution example

---
+++ src/main/java/callableFuture/CallableFutureSample.java
@@ -0,0 +1,28 @@
+package callableFuture;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CallableFutureSample {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        long nanoTime = System.nanoTime();
+        for (int i = 0; i < 10; i++) {
+            CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return new Random().nextInt();
+            })
+                    .thenApply(random -> {
+                        System.out.println(random);
+                        return random;
+                    });
+        }
+        System.out.println("total time: " + (System.nanoTime() - nanoTime));
+        Thread.sleep(1000);
+        System.out.println("the end");
+    }
+}
