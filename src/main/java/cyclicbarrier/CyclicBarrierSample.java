a331f863fbb88524c797bc06712a04500016b7b0
Anton Pomanytskyi <pomanitska@ae.com>
2022-01-05 17:05:27 +0200

    Cyclic Barrier Sample

---
+++ src/main/java/cyclicbarrier/CyclicBarrierSample.java
@@ -0,0 +1,44 @@
+package cyclicbarrier;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CyclicBarrierSample {
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
+
+        executorService.submit(new Task(cyclicBarrier));
+        executorService.submit(new Task(cyclicBarrier));
+        executorService.submit(new Task(cyclicBarrier));
+
+        Thread.sleep(3_000);
+        executorService.shutdown();
+    }
+}
+
+
+class Task implements Runnable {
+    CyclicBarrier cyclicBarrier;
+
+    public Task(CyclicBarrier cyclicBarrier) {
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                cyclicBarrier.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Do some operation  cyclicBarrier: " + System.nanoTime());
+            break;
+        }
+    }
+}
